41eab1932c0afd4cf6432ba166369d48151b8efa
JDekun <jurry_k@outlook.com>
2022-12-09 16:01:25 +0800

    优化

--- fcn/train_utils/train_and_eval.py
+++ fcn/train_utils/train_and_eval.py
@@ -92,7 +92,7 @@ def train_one_epoch(args, model, optimizer, data_loader, device, epoch, lr_sched
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
     header = 'Epoch: [{}]'.format(epoch)
 
-    i = 0
+    i = 1
     K = args.GAcc
     optimizer.zero_grad()
     for image, target in metric_logger.log_every(data_loader, print_freq, header):
