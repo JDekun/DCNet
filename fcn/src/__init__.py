edad338f94f9b53ebe23004322d2e1a30e753e66
JDekun <jurry_k@outlook.com>
2023-03-13 15:46:57 +0800

    将deeeplabv3 model添加的fcn工程中

--- fcn/src/__init__.py
+++ fcn/src/__init__.py
@@ -1,2 +1,3 @@
 from .fcn_model import fcn_resnet50, fcn_resnet101
 from .dc_net import dcnet_resnet50, dcnet_resnet101
+from .deeplabv3_model import deeplabv3_resnet50, deeplabv3_resnet101, deeplabv3_mobilenetv3_large
--- fcn/train_multi_GPU.py
+++ fcn/train_multi_GPU.py
@@ -4,7 +4,7 @@ import datetime
 
 import torch
 
-from src import fcn_resnet50, dcnet_resnet50,dcnet_resnet101
+from src import fcn_resnet50, dcnet_resnet50, dcnet_resnet101, deeplabv3_resnet101
 from train_utils import train_one_epoch, evaluate, create_lr_scheduler, init_distributed_mode, save_on_master, mkdir
 from my_dataset import VOCSegmentation
 import transforms as T
