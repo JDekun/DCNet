75487bd80d9711ead0cd4d47efa44514278b36c7
JDekun <jurry_k@outlook.com>
2023-03-14 17:40:19 +0800

    debug

--- fcn/train_utils/simsiam_loss.py
+++ fcn/train_utils/simsiam_loss.py
@@ -2,7 +2,10 @@ import torch
 import torch.nn as nn
 
 def Main_sampling(X, Y, y_hat, ignore_label: int = 255):
-    batch_size, feat_dim = X.shape[0], X.shape[-1]
+    batch_size, = X.shape[0]
+
+    X = X.permute(0, 2, 3, 1)
+    Y = Y.permute(0, 2, 3, 1)
 
     ii = 0
     this_y_hat = y_hat[0]
@@ -19,7 +22,7 @@ def Main_sampling(X, Y, y_hat, ignore_label: int = 255):
         SAM_X = X[ii, indices, :].squeeze(1)
         SAM_Y = Y[ii, indices, :].squeeze(1)
 
-        print(X)
+        print(X.shape)
         print(X_.shape)
         print(SAM_X.shape)
 
