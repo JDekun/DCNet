de7135e5774bbad04dfeeace4ed25b9ce3256be0
JDekun <jurry_k@outlook.com>
2023-03-14 18:40:06 +0800

    debug

--- fcn/train_utils/simsiam_loss.py
+++ fcn/train_utils/simsiam_loss.py
@@ -10,9 +10,10 @@ def Main_sampling(X, Y, y_hat, ignore_label: int = 255):
     ii = 0
     this_y_hat = y_hat[0]
     indices = (this_y_hat != ignore_label).nonzero()
-    X_ = X[ii, indices, :].squeeze(1)
-    Y_ = Y[ii, indices, :].squeeze(1)
+    X_ = X[ii, indices, :]
+    Y_ = Y[ii, indices, :]
 
+    print(X.shape)
     print(X_.shape)
 
     for ii in range(batch_size-1):
