8384f19d4f081f8f76d1ea1b68b04e9efb4bbdf5
JDekun <jurry_k@outlook.com>
2023-03-14 17:20:58 +0800

    debug

--- fcn/train_utils/simsiam_loss.py
+++ fcn/train_utils/simsiam_loss.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 def Main_sampling(X, Y, y_hat, ignore_label: int = 255):
     batch_size, feat_dim = X.shape[0], X.shape[-1]
 
+    ii = 0
     this_y_hat = y_hat[0]
     indices = (this_y_hat != ignore_label).nonzero()
     X_ = X[ii, indices, :].squeeze(1)
