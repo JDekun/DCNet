0d070f5dd980a6c313b3a398a38e9c8d8174c1b1
JDekun <jurry_k@outlook.com>
2023-04-03 20:32:42 +0800

    添加self_pace_step

--- train_utils/loss_manage/SamplesModel/__init__.py
+++ train_utils/loss_manage/SamplesModel/__init__.py
@@ -1,3 +1,3 @@
 from .sample_manage import Sampling
-from .self_pace import self_pace3, self_pace_epochs
+from .self_pace import self_pace3, self_pace_epochs, self_pace_step
 from .common_sample import only_esay, pred_ave, label_ave
--- train_utils/loss_manage/SamplesModel/self_pace.py
+++ train_utils/loss_manage/SamplesModel/self_pace.py
@@ -1,14 +1,38 @@
 import torch
 
+def self_pace_step(epoch, epochs, this_y_hat, this_y, cls_id):
+    hard_indices = ((this_y_hat == cls_id) & (this_y != cls_id)).nonzero()
+    easy_indices = ((this_y_hat == cls_id) & (this_y == cls_id)).nonzero()
+    num_hard = hard_indices.shape[0]
+    num_easy = easy_indices.shape[0]
+
+    # >>>> 核心修改部分
+    step = (epoch/epochs)
+    rate_hard = step**0.5 # y= x^0.5
+    rate_easy = (step + 1)**(-2)  # y= (x+1)^-2
+ 
+    num_hard_keep = round(rate_hard * num_hard)
+    num_easy_keep = round(rate_easy * num_easy)
+    # <<<<
+
+    perm = torch.randperm(num_hard)
+    hard_indices = hard_indices[perm[:num_hard_keep]]
+    perm = torch.randperm(num_easy)
+    easy_indices = easy_indices[perm[:num_easy_keep]] 
+    indices = torch.cat((hard_indices, easy_indices), dim=0)
+    
+    return indices
+
 def self_pace_epochs(epoch, epochs, this_y_hat, this_y, cls_id):
     hard_indices = ((this_y_hat == cls_id) & (this_y != cls_id)).nonzero()
     easy_indices = ((this_y_hat == cls_id) & (this_y == cls_id)).nonzero()
     num_hard = hard_indices.shape[0]
     num_easy = easy_indices.shape[0]
 
-    # >>>>
+    # >>>> 核心修改部分
     rate_hard = (epoch/epochs)
-    rate_easy_threshold = (1 - rate_hard) if (1 - rate_hard) > 1/4 else 1/4
+    easy_hard = 1 - rate_hard
+    rate_easy_threshold = easy_hard if easy_hard > 1/4 else 1/4
     num_hard_keep = round(rate_hard * num_hard)
     num_easy_keep = round(rate_easy_threshold * num_easy)
     # <<<<
