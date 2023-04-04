c593543abd530d1791cd9f25001c5a2389bbe1d8
JDekun <jurry_k@outlook.com>
2023-04-04 10:57:50 +0800

    增加self_pace_sample采样方式

--- train_utils/loss_manage/SamplesModel/__init__.py
+++ train_utils/loss_manage/SamplesModel/__init__.py
@@ -1,3 +1,3 @@
 from .sample_manage import Sampling
-from .self_pace import self_pace3, self_pace_epochs, self_pace_step
+from .self_pace import self_pace3, self_pace_epochs, self_pace_step, self_pace_sample
 from .common_sample import only_esay, pred_ave, label_ave
--- train_utils/loss_manage/SamplesModel/self_pace.py
+++ train_utils/loss_manage/SamplesModel/self_pace.py
@@ -1,5 +1,32 @@
 import torch
 
+def self_pace_sample(epoch, epochs, this_y_hat, this_y, cls_id):
+    hard_indices = ((this_y_hat == cls_id) & (this_y != cls_id)).nonzero()
+    easy_indices = ((this_y_hat == cls_id) & (this_y == cls_id)).nonzero()
+    num_hard = hard_indices.shape[0]
+    num_easy = easy_indices.shape[0]
+    sum = num_hard + num_easy
+
+    # >>>> 核心修改部分
+    n = 4
+    easy_rate = (num_easy/sum) # ↑
+    hard_rate = (num_hard/sum) # ↓
+
+    easy = (easy_rate + 1)**(-n)  # y= (x+1)^-2
+    hard = (hard_rate + 1)**(-n)  # y= (x+1)^-2
+
+    num_hard_keep = round(num_hard * hard)
+    num_easy_keep = round(num_easy * easy)
+    # <<<<
+
+    perm = torch.randperm(num_hard)
+    hard_indices = hard_indices[perm[:num_hard_keep]]
+    perm = torch.randperm(num_easy)
+    easy_indices = easy_indices[perm[:num_easy_keep]] 
+    indices = torch.cat((hard_indices, easy_indices), dim=0)
+    
+    return indices
+
 def self_pace_step(epoch, epochs, this_y_hat, this_y, cls_id):
     hard_indices = ((this_y_hat == cls_id) & (this_y != cls_id)).nonzero()
     easy_indices = ((this_y_hat == cls_id) & (this_y == cls_id)).nonzero()
@@ -7,9 +34,12 @@ def self_pace_step(epoch, epochs, this_y_hat, this_y, cls_id):
     num_easy = easy_indices.shape[0]
 
     # >>>> 核心修改部分
+    n = 2
     step = (epoch/epochs)
-    rate_hard = step**0.5 # y= x^0.5
-    rate_easy = (step + 1)**(-2)  # y= (x+1)^-2
+    rate_hard = step**(1/n) # y= x^0.5
+    rate_easy = (step + 1)**(-n)  # y= (x+1)^-2
+    print("rate_hard", rate_hard)
+    print("rate_easy", rate_easy)
  
     num_hard_keep = round(rate_hard * num_hard)
     num_easy_keep = round(rate_easy * num_easy)
