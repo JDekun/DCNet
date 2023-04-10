a4aed2d34014b1c4a22194c2100dba26e8359635
JDekun <jurry_k@outlook.com>
2023-04-10 09:39:59 +0800

    resume

--- train_multi_GPU.py
+++ train_multi_GPU.py
@@ -97,9 +97,10 @@ def main(args):
     if args.wandb and (args.rank in [-1, 0]):
         os.environ["WANDB_API_KEY"] = 'ae69f83abb637683132c012cd248d4a14177cd36'
         os.environ['WANDB_MODE'] = args.wandb_model
-        wandb.init(project="DCNet")
         if args.resume and args.run_name:
-            wandb.init(name=args.run_name)
+            wandb.init(project="DCNet", name=args.run_name)
+        else:
+            wandb.init(project="DCNet")
         wandb.config.update(args)
         wandb.watch(model, log="all", log_freq=10) # 上传梯度信息
 
