201a796c2f0a463bc7da62463d620319520ecac4
JDekun <jurry_k@outlook.com>
2023-04-11 21:30:01 +0800

    de

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,5 +1,6 @@
-from .aspp_contrast import aspp_contrast_resnet50, aspp_contrast_resnet101
 from .dc_net import dcnet_resnet50, dcnet_resnet101
 from .deeplabv3_model import deeplabv3_resnet50, deeplabv3_resnet101, deeplabv3_mobilenetv3_large
 from .fcn_model import fcn_resnet50, fcn_resnet101
 from .model_build import create_model
+from .aspp_contrast import aspp_contrast_resnet50, aspp_contrast_resnet101
+from .mep import mep_resnet50, mep_resnet101
