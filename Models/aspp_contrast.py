9988a019720567962bbb665cf929f02a3dac7811
JDekun <jurry_k@outlook.com>
2023-04-12 15:54:54 +0800

    de

--- Models/aspp_contrast.py
+++ Models/aspp_contrast.py
@@ -132,10 +132,10 @@ class DeepLabV3(nn.Module):
                 aspp_one = self.attention(aspp_one)
                 aspp_two = self.attention(aspp_two)
                 aspp_three = self.attention(aspp_three)
-            elif self.attention_name == "selfattention":
+            elif "selfattention" in self.attention_name:
                 aspp_one = self.attention(aspp_three, aspp_two, aspp_one)
 
-            if self.attention == "selfattention":
+            if "selfattention" in self.attention_name:
                 result["L1"] = [aspp_one, aspp_one]
             else:
                 result["L1"] = [aspp_one, aspp_two]
@@ -312,8 +312,9 @@ def aspp_contrast_resnet50(args, aux, num_classes=21, pretrain_backbone=False):
         contrast = contrast_head(256, args.project_dim)
         if attention_name == "cbam":
             attention = CBAMBlock(channel=128,reduction=8,kernel_size=7)
-        elif attention_name == "selfattention":
-            attention = ScaledDotProductAttention(d_model=128, d_k=128, d_v=128, h=1)
+        elif "selfattention" in attention_name:
+            head = int(attention_name.split("_")[1])
+            attention = ScaledDotProductAttention(d_model=128, d_k=128, d_v=128, h=head)
 
     aux_classifier = None
     # why using aux: https://github.com/pytorch/vision/issues/4292
