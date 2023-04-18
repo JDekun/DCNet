ba1079ce0834332ae7453b7fee5dfede7469e726
JDekun <jurry_k@outlook.com>
2023-04-18 12:26:36 +0800

    添加weight_ade_softmax

--- train_utils/loss_manage/SamplesModel/weight_ade.py
+++ train_utils/loss_manage/SamplesModel/weight_ade.py
@@ -1,4 +1,4 @@
-import torch
+import math
 
 def weight_ade(this_y_hat, this_y, cls_id, ade):
     hard_indices = ((this_y_hat == cls_id) & (this_y != cls_id)).nonzero()
@@ -10,12 +10,32 @@ def weight_ade(this_y_hat, this_y, cls_id, ade):
     # >>>> 核心修改部分
     n = ade
     easy_rate = (num_easy/sum) # ↑
-    hard_rate = (num_hard/sum) # ↓
+    hard_rate = 1-easy_rate # ↓
 
     hard = (hard_rate + 1)**(-n)  # y= (x+1)^-8
     easy = (easy_rate + 1)**(-n)  # y= (x+1)^-8
 
     # <<<<
     
+    return hard_indices, easy_indices, hard, easy
+
+
+def weight_ade_softmax(this_y_hat, this_y, cls_id, ade):
+    hard_indices = ((this_y_hat == cls_id) & (this_y != cls_id)).nonzero()
+    easy_indices = ((this_y_hat == cls_id) & (this_y == cls_id)).nonzero()
+    num_hard = hard_indices.shape[0]
+    num_easy = easy_indices.shape[0]
+    sum = num_hard + num_easy
+
+    # >>>> 核心修改部分
+    n = ade
+    easy_rate = (num_easy/sum) # ↑ 简单样本的比例在上升
+    hard_weight = easy_rate # ↓ 难样本的权重在上升
+    easy_weight = 1-easy_rate # ↓ 简单样本的权重在下降
+
+    hard = math.exp(hard_weight)/(math.exp(hard_weight)+math.exp(easy_weight))
+    easy = math.exp(easy_weight)/(math.exp(hard_weight)+math.exp(easy_weight))
+
+    # <<<<
     
     return hard_indices, easy_indices, hard, easy
